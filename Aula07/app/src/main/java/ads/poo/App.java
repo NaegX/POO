672eb704f03065620c473c75a7448489171da04f
Gean Luccas <gean.x@aluno.ifsc.edu.br>
2024-03-12 22:35:07 -0300

    Aula7

---
+++ Aula07/app/src/main/java/ads/poo/App.java
@@ -0,0 +1,32 @@
+package ads.poo;
+
+public class App {
+    
+    public static void main(String[] args){
+
+    //     Buzz b = new Buzz();
+
+    //     System.out.println(b.dispararLases());
+
+    //     System.out.println("Capacete aberto? " + b.isCapaceteAberto());
+    //     b.abrirFecharCapacete();
+    //     System.out.println("Capacete aberto? " + b.isCapaceteAberto());
+
+    //     Buzz malvado = new Buzz();
+
+    //     System.out.println("Capacete aberto? " + malvado.isCapaceteAberto());
+
+    //     System.out.println(b.golpear() + " Buzz malvado");
+    //     System.out.println(malvado.golpear() + " Buzz bonzinho");
+    // }
+    // public static void main(String[] args){
+    //     Caneta c = new Caneta();
+    //     c.setCor("verde");
+        Pessoa Juca = new Pessoa();
+        Juca.setCpf("035.365.569-00");
+        Juca.setEmail("juca@gmail.com");
+        Juca.setName("Juca Pato");
+        System.out.println(Juca);
+
+    }
+}
