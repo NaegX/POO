af5130502b779c7a98f03ac4a3f6ae1b598be7ec
Gean Luccas <gean.x@aluno.ifsc.edu.br>
2024-08-20 22:11:00 -0300

    aula de Logs e Lombok

---
+++ Registro/app/src/main/java/ads/poo/Pessoa.java
@@ -0,0 +1,27 @@
+// package ads.poo;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NonNull;
+// import lombok.ToString;
+// import lombok.extern.slf4j.Slf4j;
+
+// @ToString(exclude =  {"cpf", "dataNascimento"})
+
+// @Slf4j
+
+// @AllArgsConstructor
+// @Data
+// public class Pessoa {
+
+//     @NonNull
+//     private String nome;
+
+//     @NonNull
+//     private String cpf;
+
+//     private LocalDate dataNascimento;
+
+//     public void validadeCpf ("Validando cpf: {}" cpf);
+    
+// }
